f2b781b00dcc27674bdb106a494fe742e0db966e
Arnaud <arnaud.chane.dev@gmail.com>
2024-04-08 16:09:19 +0200

    WIP - boardlist Service create boardlist

---
+++ src/app/Service/boardlist.service.ts
@@ -0,0 +1,29 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { AuthService } from "./auth.service";
+import { BoardListDto } from "../model/boardListDto";
+import { Observable } from "rxjs";
+
+@Injectable({
+    providedIn: 'root'
+})
+
+export class BoardlistService {
+    constructor(private http: HttpClient, private auth: AuthService) {
+        this.currentBoardlistDto = {
+            name: '',
+            projectId: 0
+        }
+     }
+
+    boardlistServiceUrl = 'http://localhost:8081/api/boardlist';
+    currentBoardlistDto: BoardListDto;
+    currentBoardlist?: BoardListDto;
+    userBoardlists: BoardListDto[] = [];
+
+    //create a boardlist
+    createBoardlist(boardlist: BoardListDto): Observable<BoardListDto> {
+        return this.http.post<BoardListDto>(this.boardlistServiceUrl + '/create', boardlist);
+    }
+
+}
