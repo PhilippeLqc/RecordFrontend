afd40ffe59c00b7b7933e88135cf275152f974fe
Arnaud <arnaud.chane.dev@gmail.com>
2024-04-10 15:55:57 +0200

    Create a boardlist and display it

--- src/app/Service/task.service.ts
+++ src/app/Service/task.service.ts
@@ -1,9 +1,43 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { TaskDto } from '../model/taskDto';
+import { Status } from '../enumTypes/status';
+import { Hierarchy } from '../enumTypes/hierarchy';
+import { Observable, tap } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
 
-  constructor() { }
+    boardlistServiceUrl = 'http://localhost:8081/api/task';
+    // currentTaskDto: TaskDto;
+    currentTask?: TaskDto;
+    allTasksOfBoardlist: TaskDto[] = [];
+
+    constructor(private http: HttpClient, private route: ActivatedRoute) {
+      //   this.currentTaskDto = {
+      //     id: 0,
+      //     title: "",
+      //     description: "",
+      //     expirationDate: Date,
+      //     status: Status.ACTIVE,
+      //     hierarchy: Hierarchy.IMPORTANT,
+      //     userId: number[],
+      //     boardListId: number,
+    }
+
+    // Get all tasks by boardlist id
+    getTasksByBoardlistId(boardlistId: number): Observable<TaskDto[]> {
+        return this.http.get<TaskDto[]>(`${this.boardlistServiceUrl}/boardlist/${boardlistId}`).pipe(
+            tap((response) => {
+                console.log("REPONSE DATA DE GETTASKSBYBOARDLISTID");
+                console.log(response);
+                this.allTasksOfBoardlist = response;
+                console.log("END DATA DE GETTASKSBYBOARDLISTID");
+            })
+        );
+    }
 }
