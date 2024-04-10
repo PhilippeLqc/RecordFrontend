fbff12224ec607e2ff285d74805e4a80aad11d9f
Arnaud <arnaud.chane.dev@gmail.com>
2024-04-10 11:03:03 +0200

    Fixing merge rebase

--- src/app/Service/boardlist.service.ts
+++ src/app/Service/boardlist.service.ts
@@ -1,33 +1,57 @@
-import { HttpClient } from "@angular/common/http";
-import { Injectable } from "@angular/core";
-import { AuthService } from "./auth.service";
-import { BoardListDto } from "../model/boardListDto";
-import { Observable, tap } from "rxjs";
+    import { HttpClient } from "@angular/common/http";
+    import { Injectable } from "@angular/core";
+    import { AuthService } from "./auth.service";
+    import { BoardListDto } from "../model/boardListDto";
+    import { Observable, tap } from "rxjs";
+    import { ActivatedRoute } from "@angular/router";
 
-@Injectable({
-  providedIn: 'root'
-})
-export class BoardlistService {
-    constructor(private http: HttpClient, private auth: AuthService) {
-        this.currentBoardlistDto = {
-            name: '',
-            projectId: 0
+    @Injectable({
+        providedIn: 'root'
+    })
+
+    export class BoardlistService {
+        constructor(private http: HttpClient, private auth: AuthService, private route: ActivatedRoute) {
+            this.currentBoardlistDto = {
+                name: '',
+                projectId: this.projectId
+            }
         }
-     }
 
-    boardlistServiceUrl = 'http://localhost:8081/api/boardlist';
-    currentBoardlistDto: BoardListDto;
-    currentBoardlist?: BoardListDto;
-    userBoardlists: BoardListDto[] = [];
+        boardlistServiceUrl = 'http://localhost:8081/api/boardlist';
+        currentBoardlistDto: BoardListDto;
+        currentBoardlist?: BoardListDto;
+        allBoardlistsOfProject: BoardListDto[] = [];
+        projectId: number = Number(this.route.snapshot.paramMap.get('projectId'));
 
-    //create a boardlist
-    createBoardlist(boardlist: BoardListDto): Observable<BoardListDto> {
-        return this.http.post<BoardListDto>(this.boardlistServiceUrl + '/create', boardlist).pipe(
-            tap((response) => {
-                this.currentBoardlist = response
-            })
-        );
+        //create a boardlist
+        createBoardlist(boardlist: BoardListDto, projectId: number): Observable<BoardListDto> {
+            return this.http.post<BoardListDto>(`${this.boardlistServiceUrl}/create/${projectId}`, boardlist).pipe(
+                tap((response) => {
+                    console.log("REPONSE DATA DE CREATEBOARDLIST");
+
+                    console.log(response);
+                    this.currentBoardlist = response
+                    console.log("END DATA DE CREATEBOARDLIST");
+
+                })
+            );
+        }
+
+        //get all boardlists by project id
+        getBoardlistsByProjectId(projectId: number): Observable<BoardListDto[]> {
+            console.log(this.boardlistServiceUrl + '/project/' + this.projectId);
+
+            return this.http.get<BoardListDto[]>(`${this.boardlistServiceUrl}/project/${projectId}`).pipe(
+                tap((response) => {
+                    console.log("REPONSE DATA DE CREATEBOARDLIST");
+
+                    console.log(response);
+                    this.allBoardlistsOfProject = response;
+                    console.log("END DATA DE CREATEBOARDLIST");
+
+                })
+            );
+        }
+
     }
 
-  constructor() { }
-}
--- src/app/components/boardlist/boardlist.component.ts
+++ src/app/components/boardlist/boardlist.component.ts
@@ -1,12 +1,64 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
+import { BoardlistService } from '../../Service/boardlist.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { BoardListDto } from '../../model/boardListDto';
+import { merge } from 'rxjs';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-boardlist',
   standalone: true,
   imports: [],
   templateUrl: './boardlist.component.html',
-  styleUrl: './boardlist.component.css'
+  styleUrl: './boardlist.component.css',
 })
-export class BoardlistComponent {
 
+export class BoardlistComponent implements OnInit {
+  constructor(
+    public boardlistS: BoardlistService,
+    private formBuilder: FormBuilder,
+    private route: ActivatedRoute
+  ) {
+    merge(this.name.statusChanges)
+      .pipe(takeUntilDestroyed())
+      .subscribe(() => this.updateErrorName());
+  }
+
+  // In BoardlistComponent
+  ngOnInit(): void {
+    const projectId = Number(this.route.snapshot.paramMap.get('projectId'));
+    this.boardlistS.getBoardlistsByProjectId(projectId).subscribe(() => {
+      this.boardlistsProject = this.boardlistS.allBoardlistsOfProject;
+    });
+  }
+
+  boardlistsProject: BoardListDto[] = [];
+  name = new FormControl('', Validators.required);
+  errorMessage = '';
+  projectId: Number = Number(this.route.snapshot.paramMap.get('projectId'));
+
+  public boardlistForm = this.formBuilder.group({
+    name: [''],
+    projectId: this.projectId,
+  });
+
+  updateErrorName(): void {
+    if (this.name.hasError('required')) {
+      this.errorMessage = 'You must enter a name';
+    }
+  }
+
+  onSubmit() {
+    const projectId = Number(this.route.snapshot.paramMap.get('projectId'));
+    let boardlist = {
+      name: this.boardlistForm.value.name!,
+      projectId: projectId,
+    };
+    this.boardlistS
+      .createBoardlist(boardlist, projectId)
+      .subscribe((newBoardlist) => {
+        this.boardlistsProject.push(newBoardlist);
+      });
+  }
 }
