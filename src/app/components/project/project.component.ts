7fc2ef2e3e69972769b751c94cfdd2eaff89d9ac
Arnaud <arnaud.chane.dev@gmail.com>
2024-04-10 10:56:51 +0200

    Fixing rebase

--- src/app/components/project/project.component.ts
+++ src/app/components/project/project.component.ts
@@ -42,13 +42,14 @@ export class ProjectComponent implements OnInit{
     this.project.getProjectsByUserId().subscribe(() => {
       console.log(this.project.userProjects);
       //usefull ?
-      localStorage.setItem('userProjects', JSON.stringify(this.project.userProjects));
+     localStorage.setItem('userProjects', JSON.stringify(this.project.userProjects));
     });
 
     this.userProjects = JSON.parse(localStorage.getItem('userProjects') || '{}');
   }
   
   userProjects: ProjectDto[] = [];
+  projectServiceUrl = 'http://localhost:8081/api/project';
   title = new FormControl('', Validators.required);
   errorMessage = '';
 
