79c7b2610a2a54a0448d8da118d18e9adaddac22
Arnaud-Chane <arnaud.chane.dev@gmail.com>
2024-04-21 15:56:23 +0200

    Adjust animation timing and on which pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,13 @@ module.exports = {
   theme: {
     extend: {
       animation: {
-        drop: 'drop 2s ease-in-out',
-        rise: 'rise 2s ease-in-out',
-        slideInRight: 'slideInRight 2s ease-in-out',
+        dropHomePage: 'drop 2s ease-in-out',
+        riseHomePage: 'rise 2s ease-in-out',
+        slideInRightHomePage: 'slideInRight 2s ease-in-out',
+
+        drop: 'drop 1s ease-in-out',
+        slideInRight: 'slideInRight 1s ease-in-out',
+        slideInLeft: 'slideInLeft 1s ease-in-out',
       },
       keyframes: {
         drop: {
@@ -19,6 +23,10 @@ module.exports = {
         },
         slideInRight: {
           '0%': { opacity: '0', transform: 'translateX(50px)' },
+          '100%': { opacity: '1', transform: 'translateX(0)' },
+        },
+        slideInLeft: {
+          '0%': { opacity: '0', transform: 'translateX(-50px)' }, // Changed from translateX(50px)
           '100%': { opacity: '1', transform: 'translateX(0)' },
         },
       }
