20244840b77b84441862a83cf61f5074daccc0d5
Arnaud-Chane <arnaud.chane.dev@gmail.com>
2024-04-21 14:26:49 +0200

    Adding animation and logo in HomePage

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        drop: 'drop 2s ease-in-out',
+        rise: 'rise 2s ease-in-out'
+      },
+      keyframes: {
+        drop: {
+          '0%': { opacity: '0', transform: 'translateY(-50px)' },
+          '100%': { opacity: '1', transform: 'translateY(0)' },
+        },
+        rise: {
+          '0%': { opacity: '0', transform: 'translateY(50px)' },
+          '100%': { opacity: '1', transform: 'translateY(0)' },
+        }
+      }
+    }
   },
+  variants: {},
   plugins: [],
 };
