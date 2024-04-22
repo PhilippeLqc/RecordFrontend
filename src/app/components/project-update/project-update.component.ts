910ef82effba1eb7fe16fd4c2af792ddc9fcc6a1
Arnaud <arnaud.chane.dev@gmail.com>
2024-04-22 13:55:44 +0200

    Generate project update

---
+++ src/app/components/project-update/project-update.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-project-update',
+  standalone: true,
+  imports: [],
+  templateUrl: './project-update.component.html',
+  styleUrl: './project-update.component.css'
+})
+export class ProjectUpdateComponent {
+
+}
