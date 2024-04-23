3ad3fc5fec8d48535e0cd23ac95f1aff0dbbb87f
Philippe Le Quoc <philippe.lequoc@gmail.com>
2024-04-23 16:22:02 +0200

    Ajout des fonctionnalités de modification du mot de passe et de l'e-mail dans le composant Profile

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './components/homepage/home.component';
 import { ProjectComponent } from './components/project/project.component';
 import { ProjectDetailsComponent } from './components/project-details/project-details.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
 export const routes: Routes = [
     {path: 'home', component: HomeComponent},
     {path: 'project', component: ProjectComponent},
     {path: 'project/:projectId', component: ProjectDetailsComponent},
+    {path: 'profile', component: ProfileComponent},
     {path: '', redirectTo: '/home', pathMatch: 'full'}
 ];
